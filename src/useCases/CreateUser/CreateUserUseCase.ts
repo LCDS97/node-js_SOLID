f2255b303bbf9f8910ba82a243e01f6114147f81
LCDS97 <lucas.cds1997@gmail.com>
2021-08-24 00:07:33 -0300

    Create User Feature

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,5 +1,22 @@
+import { IUsersRepository } from "../../repositories/IUserRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+import { User } from "../../entities/User";
+
 export class CreateUserUseCase {
-    async execute(){
-        
+
+    constructor(
+       private usersRepository: IUsersRepository
+    ) {}
+
+    async execute(data: ICreateUserRequestDTO){
+        const userAlreadyExists = await this.usersRepository.findByEmail(data.email);
+
+        if (userAlreadyExists) {
+            throw new Error('User already exists')
+        }
+
+        const user = new User(data);
+
+        await this.usersRepository.save(user);
     }
 }
